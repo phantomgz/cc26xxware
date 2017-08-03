4676ffe3e07e366e01d077881ee8e070e0d884b8
caicx <caichaoxi@gmail.com>
2017-08-03 16:07:02 +0800

    fixed ChipInfo_GetChipType() can not detect CC2630 correctly, because missing a "break" .

--- driverlib/chipinfo.c
+++ driverlib/chipinfo.c
@@ -145,6 +145,7 @@ ChipInfo_GetChipType( void )
       case 0x4 :
       case 0xC :
          chipType = CHIP_TYPE_CC2630 ;
+         break;	// missing in "v2.24.03.17272"
       case 0x1 :
       case 0x9 :
          chipType = CHIP_TYPE_CC2640 ;
